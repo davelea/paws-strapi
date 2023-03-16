70a8bcdcc6d4bf8b3ca38035be27b53fe857df71
Dave Lea <davegt40@gmail.com>
2023-03-15 23:37:45 -0400

    upgrade steps

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '06afff21eb4766dc0a59461bc3b67a93'),
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
